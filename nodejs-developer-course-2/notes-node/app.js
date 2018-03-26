f3f65a3fbcdb4e5d123147cb2250210992eb3606
Mike Tran <xage283@gmail.com>
2018-03-25 17:52:52 -0700

    Finished Section 3, Lecture 14

--- nodejs-developer-course-2/notes-node/app.js
+++ nodejs-developer-course-2/notes-node/app.js
@@ -2,22 +2,25 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const command = process.argv[2];
+const argv = yargs.argv;
 
-console.log(process.argv);
+const command = argv._[0];
+
 console.log('Command:', command);
+console.log('yargs', argv);
 
 if (command === 'add') {
-    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('Listing all notes');
+    notes.getAll();
 } else if (command === 'read') {
-    console.log('Reading note...');
+    notes.getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Removing note');
+    notes.remove(argv.title);
 } else {
     console.log('Unknown command:', command);
 }
--- nodejs-developer-course-2/notes-node/notes.js
+++ nodejs-developer-course-2/notes-node/notes.js
@@ -1,6 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+var addNote = (title, body) => {
+    console.log(`Adding note, title: ${title}, body: ${body}`);
+};
+
+var getAll = () => {
+    console.log('All your notes are belong to us!');
+};
+
+var getNote = (title) => {
+    console.log(`Reading ${title}`);
+};
+
+var remove = (title) => {
+    console.log(`Removing ${title}`);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    remove,
 };
