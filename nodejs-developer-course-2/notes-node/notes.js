bcecd50c3efee05b4e13c27e744d239108085ad1
Mike Tran <xage283@gmail.com>
2018-03-25 23:01:20 -0700

    Finished Section 3, Lecture 16

--- nodejs-developer-course-2/notes-node/notes.js
+++ nodejs-developer-course-2/notes-node/notes.js
@@ -1,7 +1,29 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+const notesFileName = 'notes-data.json';
+
 var addNote = (title, body) => {
-    console.log(`Adding note, title: ${title}, body: ${body}`);
+    var notes = [];
+    const note = {
+        title,
+        body,
+    };
+
+    try {
+        const notesString = fs.readFileSync(notesFileName);
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    const duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync(notesFileName, JSON.stringify(notes));
+    }
 };
 
 var getAll = () => {
